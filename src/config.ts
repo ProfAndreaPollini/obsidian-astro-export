147525d0ca7f254aebcf712ff4748c333442e7d9
Andrea Pollini <prof.andrea.pollini@gmail.com>
2023-04-03 11:36:42 +0200

    version 1.0.0 - first release

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 
 export interface AstroPublishSettings {
 	outContentFolder: string;
+	isWorking: boolean;
 	foldersToWatch: string[];
 }
 
 export const DEFAULT_SETTINGS: AstroPublishSettings = {
 	outContentFolder: 'default',
+	isWorking: false,
 	foldersToWatch: ['default'],
 }
